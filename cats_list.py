7592c062355c779b101b9179f066ee6c6bdad41d
Regina0101 <ritodo1010@gmai.com>
2024-02-06 17:27:46 +0000

    added comments to cats_list

--- cats_list.py
+++ cats_list.py
@@ -6,11 +6,13 @@ def get_cats_info(path):
     with open(path, 'r') as fh:
         content = fh.readlines()
 
+    '''New list of keys'''
     keys_list=["id", "name", "age"]
     cats_info=[]
     for lines in content:
         line = lines.split(',')
         cleaned_list = [item.replace('\n', '') for item in line]
+        '''Zip list and dict together to create dict'''
         cats_list = dict(zip(keys_list, cleaned_list))
         cats_info.append(cats_list)
 
